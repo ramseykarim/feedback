de049821c56cfdfbae07bee8d9e5c9be46c5cc43
Ramsey Karim <rkarim@astro.umd.edu>
2019-11-14 15:50:17 -0500

    finished basic stellar type regex

--- readstartypes.py
+++ readstartypes.py
@@ -19,6 +19,13 @@ spectypes_directory = f"{catalogs_directory}SpecTypes/"
 spectypes_table = f"{spectypes_directory}spectypes.txt"
 column_name_table = f"{spectypes_directory}colnames.txt"
 
+letter_re = '((W(N|C))|[OBAFGKM])'
+roman_num_re = '(I+|I?V)'
+peculiar_re = '(((ha)|n|(\\(*(f|e)(\\+|\\*)?\\)*))+\\*?)'
+number_re = '(\\d{1}(\\.\\d)?)'
+slashdash_re = '(/|-)'
+
+
 def table_name(spectral_subtype):
     # Spectral subtypes V, III, and I are available
     return f"{spectypes_directory}class{spectral_subtype}.txt"
@@ -41,55 +48,52 @@ def generate_SpectralType_DataFrame():
     return spectraltype_dfs, col_units
 
 
-def ST_parse_type(spectral_type_string):
-    # Get type and subtype from spectral type, i.e. O4.5III or B1 V
-    subtype = None
-    for st in spectral_subtypes:
-        if st in spectral_type_string:
-            subtype = st
-            spectral_type_string = spectral_type_string.replace(st, '')
-            break
-    letter = spectral_type_string[0]
-    number, i = [], 1
-    while i < len(spectral_type_string) and spectral_type_string[i] in '1234567890.':
-        number.append(spectral_type_string[i])
-        i += 1
-    return f"{letter}{''.join(number)}", subtype
+def re_parse_helper(pattern, string):
+    # Return match string if it exists, else empty string
+    match = re.search(pattern, string)
+    if match:
+        return match.group()
+    else:
+        return ''
 
 
-# Then remove f's and *'s and stuff like that
+def st_parse_type(spectral_type_string):
+    # Parse a single full class string (no slashes or dashes)
+    # i.e. O5.5III((f))* => 'O', '5.5', 'III', '((f))*'
+    pec = re_parse_helper(peculiar_re, spectral_type_string)
+    lumclass = re_parse_helper(roman_num_re, spectral_type_string)
+    subtype = re_parse_helper(number_re, spectral_type_string)
+    lettertype = re_parse_helper(letter_re, spectral_type_string)
+    return (lettertype, subtype, lumclass, pec)
 
-# Then split up the luminosity class from the spectral type
 
-
-def ST_parse_slash(spectral_type_string):
-    if '/' not in spectral_type_string:
+def st_parse_slashdash(spectral_type_string):
+    if not re.search(slashdash_re, spectral_type_string):
         # Check for / and return list(string) if not
         return [spectral_type_string]
-    roman_num = '(I+|I?V)'
-    peculiar = '(\\(*(f|e)(\\+|\\*)?\\)*\\*?)'
-    number = '(\\d{1}(\\.\\d)?)'
     # Parse the slash notation from SINGLE star (not binary)
     # The O/WN stars are a confusing category: see Wikipedia Wolf-Rayet stars: Slash stars
-    if spectral_type_string[spectral_type_string.index('/') + 1] == 'W':
+    elif '/' in spectral_type_string and spectral_type_string[spectral_type_string.index('/') + 1] == 'W':
         # This is a "slash star", return both complete spectral types
         return spectral_type_string.split('/')
     # Add possible luminosity classes to this list
     possible_lumclasses = []
-    pattern_lumclass = f'({roman_num}{peculiar}?)(/({roman_num}{peculiar}?))+'
+    pattern_lumclass = f'{roman_num_re}{peculiar_re}?({slashdash_re}{roman_num_re}{peculiar_re}?)+'
     match_lumclass = re.search(pattern_lumclass, spectral_type_string)
     if match_lumclass:
-        possible_lumclasses.extend(match_lumclass.group().split('/'))
+        # print("-----", spectral_type_string, '->', match_lumclass.group())
+        possible_lumclasses.extend(match_lumclass.group().replace('-', '/').split('/'))
         for i in range(len(possible_lumclasses)):
             possible_lumclasses[i] = spectral_type_string.replace(match_lumclass.group(), possible_lumclasses[i])
     else:
         possible_lumclasses.append(spectral_type_string)
     possible_classes = []
     for possible_lumclass in possible_lumclasses:
-        pattern_subclass = f'({number})(/({number}))+'
+        pattern_subclass = f'{number_re}({slashdash_re}{number_re})+'
         match_subclass = re.search(pattern_subclass, possible_lumclass)
         if match_subclass:
-            possible_subclasses = match_subclass.group().split('/')
+            # print("-----"*2, match_subclass, '->', match_subclass.group())
+            possible_subclasses = match_subclass.group().replace('-', '/').split('/')
             for i in range(len(possible_subclasses)):
                 possible_classes.append(possible_lumclass.replace(match_subclass.group(), possible_subclasses[i]))
         else:
@@ -97,21 +101,22 @@ def ST_parse_slash(spectral_type_string):
     return possible_classes
 
 
-
-def ST_parse_full(spectral_type_string):
-    # Work from broadest issue to most specific issues
+def st_parse_binary(spectral_type_string):
     # Identify binaries (+)
-    stars = spectral_type_string.split('+')
+    return spectral_type_string.split('+')
 
 
-
-def ST_to_number(spectral_type):
+def st_to_number(spectral_type):
     # Returns a float number based on the spectral type, increasing with later type
     # Just simple letter+number spectral_type, decminal optional
     # Like "O3" or "B1.5"
     # Wow ok this exact system was used in Vacca et al 1996... nice one dude
     type_key = 'OBAFGKM'
     return type_key.index(spectral_type[0])*10. + float(spectral_type[1:])
+
+
+def lc_to_number(lumclass):
+    return ['I', 'II', 'III', 'IV', 'V'].index(lumclass) + 1
 
 
 def fit_characteristic(df_subtype, characteristic):
@@ -131,34 +136,30 @@ class SpectralTypeTables:
         self.memoized_interpolations = {}
         self.memoized_type_names = {}
 
-    def lookup_characteristic(spectral_class, characteristic):
-        # Spectral class includes luminosity class, i.e. O9.5III or B4V
+    def lookup_characteristic(spectral_type_tuple, characteristic):
+        # Spectral type tuple can be 3 or 4 elements (4th is peculiarity, ignored)
         # Characteristic is a valid column name, i.e. Teff or log_L
         if characteristic not in self.column_units.index:
             raise RuntimeError(f"{characteristic} is not a recognized column name.")
-        # Check if this full spectral type has been searched before, attempt to skip parsing
-        if spectral_class in self.memoized_type_names:
-            spectral_type, subtype = self.memoized_type_names[spectral_class]
-        else:
-            spectral_type, subtype = ST_parse_type(spectral_class)
-            self.memoized_type_names[spectral_class] = (spectral_type, subtype)
+        # Discard peculiarity
+        spectral_type_tuple = spectral_type_tuple[:3]
+        lettertype, subtype, lumclass = spectral_type_tuple
         # Get subtype DataFrame
-        df_subtype = self.star_tables[subtype]
+        df_subtype = self.star_tables[lumclass]
         if spectral_type in df_subtype.index:
             # If the spectral type is in this table, return value for characteristic
             return df_subtype[characteristic].loc[spectral_type]
         else:
             # If type is not in table, interpolate between types using number approximation
-            if (spectral_type, subtype) in self.memoized_interpolations:
+            if spectral_type_tuple in self.memoized_interpolations:
                 # Check if there is a saved interpolation for this type/subtype
-                interp_function = self.memoized_interpolations[(spectral_type, subtype)]
+                interp_function = self.memoized_interpolations[spectral_type_tuple]
             else:
                 # If we don't, make one
                 interp_function = fit_characteristic(df_subtype, characteristic)
-                self.memoized_interpolations[(spectral_type, subtype)] = interp_function
+                self.memoized_interpolations[spectral_type_tuple] = interp_function
             # Use interpolation to return a value for the characteristic
             return interp_function(spectral_type)
-
 
 
     def lookup_units(characteristic):
@@ -182,13 +183,16 @@ def plot_sternberg_stuff():
     plt.show()
 
 
-def test_ST_parse_slash():
+def test_ST_parse_slashdash():
     cat = pd.read_pickle(f"{catalogs_directory}/Ramsey/OBradec.pkl")
-    tests = [cat.SpectralType[19], cat.SpectralType[5], 'B5/6.5III', 'O4II/III', cat.SpectralType[26]]
+    tests = [cat.SpectralType[19], cat.SpectralType[5], cat.SpectralType[7], 'B5/6.5III', 'O4II/III', cat.SpectralType[26], cat.SpectralType[27]]
     for t in tests:
-        print(t, ST_parse_slash(t))
+        l = ST_parse_slashdash(t)
+        print(t, '\t', l)
+        print('\t', [ST_parse_type(x) for x in l])
+        print()
 
 
 
 if __name__ == "__main__":
-    test_ST_parse_slash()
+    test_ST_parse_slashdash()
