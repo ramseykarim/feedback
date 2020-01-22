efbc87ab5a6f39dd751642fbb3eabcc79f8b7cad
Ramsey Karim <rkarim@astro.umd.edu>
2020-01-21 20:53:10 -0500

    made a pkl combining function. needs to be replaced by proper fits files

--- parse_FIR_fits.py
+++ parse_FIR_fits.py
@@ -7,42 +7,72 @@ testname = "/home/rkarim/Research/mantipython/TEST.pkl"
 herschel_path = "/home/rkarim/Research/Feedback/ancillary_data/herschel/"
 # fns = ["2p_5b_freshoffsets", "3p_5b_freshoffsets", "2p_3b"]
 # fns = [f"{herschel_path}RCW49large_{fn}.pkl" for fn in fns]
-with open(herschel_path+"RCW49large_TEST.pkl", 'rb') as f:
-    fit, chisq, models, diffs = pickle.load(f)
 
 
-T, tau = fit
-p70, p160, s250, s350, s500 = diffs
-beta = np.full(T.shape, 2.0)
+def quicklook(filename):
+    with open(filename, 'rb') as f:
+        fit, chisq, models, diffs = pickle.load(f)
 
-plt.figure(figsize=(16, 9))
-plt.subplot(231)
-plt.imshow(T, origin='lower', vmin=10, vmax=30)
-plt.colorbar()
-plt.title('T')
-plt.subplot(232)
-plt.imshow(tau, origin='lower', vmin=-3, vmax=-1)
-plt.colorbar()
-plt.title('tau')
-plt.subplot(233)
-plt.imshow(beta, origin='lower', vmin=1, vmax=2.5)
-plt.colorbar()
-plt.title('beta')
+    T, tau, beta = fit
+    p70, p160, s250, s350, s500 = diffs
+    # beta = np.full(T.shape, 2.0)
 
-plt.subplot(234)
-plt.imshow(p70, origin='lower', vmin=-20, vmax=20)
-plt.colorbar()
-plt.title('model-70')
-plt.subplot(235)
-plt.imshow(p160, origin='lower', vmin=-20, vmax=20)
-plt.colorbar()
-plt.title('model-160')
-plt.subplot(236)
-plt.imshow(s350, origin='lower', vmin=-2, vmax=2)
-plt.colorbar()
-plt.title('model-350')
+    plt.figure(figsize=(16, 9))
+    plt.subplot(231)
+    plt.imshow(T, origin='lower', vmin=10, vmax=70)
+    plt.colorbar()
+    plt.title('T')
+    plt.subplot(232)
+    plt.imshow(tau, origin='lower', vmin=-3, vmax=-1)
+    plt.colorbar()
+    plt.title('tau')
+    plt.subplot(233)
+    plt.imshow(beta, origin='lower', vmin=1, vmax=2.5)
+    plt.colorbar()
+    plt.title('beta')
 
-plt.figure()
-plt.imshow(chisq, origin='lower', vmax=50)
+    plt.subplot(234)
+    plt.imshow(p70, origin='lower', vmin=-20, vmax=20)
+    plt.colorbar()
+    plt.title('model-70')
+    plt.subplot(235)
+    plt.imshow(p160, origin='lower', vmin=-20, vmax=20)
+    plt.colorbar()
+    plt.title('model-160')
+    plt.subplot(236)
+    plt.imshow(s350, origin='lower', vmin=-2, vmax=2)
+    plt.colorbar()
+    plt.title('model-350')
 
-plt.show()
+    plt.figure()
+    plt.imshow(np.log10(chisq), origin='lower', vmax=4)
+    plt.colorbar()
+    plt.show()
+
+
+def combine(fn_template):
+    with open(fn_template(1), 'rb') as f:
+        contents = pickle.load(f)
+        tile_shape = contents[1].shape
+        every_shape = tuple(x.shape for x in contents)
+    full_shape = tuple(2*x for x in tile_shape)
+    every_template = [np.full(((shape[0], *full_shape) if len(shape)>2 else full_shape), np.nan) for shape in every_shape]
+    c1 = (slice(tile_shape[0], full_shape[0]), slice(0, tile_shape[1]))
+    c2 = (slice(tile_shape[0], full_shape[0]), slice(tile_shape[1], full_shape[1]))
+    c3 = (slice(0, tile_shape[0]), slice(tile_shape[1], full_shape[1]))
+    c4 = (slice(0, tile_shape[0]), slice(0, tile_shape[1]))
+    cutouts = (c1, c2, c3, c4)
+    for i, cutout in zip(range(1, 5), cutouts):
+        with open(fn_template(i), 'rb') as f:
+            contents = pickle.load(f)
+        for target, source in zip(every_template, contents):
+            try:
+                target[:, cutout[0], cutout[1]] = source[:]
+            except:
+                target[cutout] = source[:]
+    with open(fn_template("FULL"), 'wb') as f:
+        pickle.dump(every_template, f)
+    print('wrote full map')
+
+# combine(lambda i: f"{herschel_path}RCW49large_3p_nocal_TILE{i}.pkl")
+quicklook(herschel_path+"RCW49large_3p_freshcal_TILEFULL.pkl")
