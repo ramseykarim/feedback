407598ce85da884b379445dffa1b068fc2854546
Ramsey Karim <rkarim@astro.umd.edu>
2020-04-07 16:17:41 -0400

    need to debug in atom locally

--- crosscut.py
+++ crosscut.py
@@ -190,8 +190,8 @@ if __name__ == "__main__":
 
     img, w = load_image(data_path + cuts_to_make["8 um"])
     plt.subplot(122, projection=w)
-    plt.imshow(np.arcsinh(img), origin='lower', vmin=np.arcsinh(11), vmax=np.arcsinh(900), cmap='greys_r')
+    plt.imshow(np.arcsinh(img), origin='lower', vmin=np.arcsinh(11), vmax=np.arcsinh(900), cmap='Greys_r')
     plt.arrow([coord_start_xcut.ra.to_value(), coord_end_xcut.ra.to_value()], [coord_start_xcut.dec.to_value(), coord_end_xcut.dec.to_value()],
         transform=plt.gca().get_transform('world'), color='r', length_includes_head=True, width=0.003)
     # plt.show()
-    plt.savefig(f"/home/rkarim/Pictures/4-01-20-work/crosscut_{selection}.png")
+    plt.savefig(f"/home/rkarim/Pictures/4-07-20-work/crosscut_{selection}.png")
