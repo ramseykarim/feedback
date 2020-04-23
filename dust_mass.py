a6090b8688fc7037d926926c2d84959ebfd4a94c
Ramsey Karim <rkarim@astro.umd.edu>
2020-04-23 12:38:09 -0400

    partway thru 3d dust mass images to explain how i got the numbers

--- dust_mass.py
+++ dust_mass.py
@@ -4,14 +4,21 @@ import matplotlib.pyplot as plt
 import scipy.constants as cst
 from scipy.interpolate import interp1d
 from scipy.special import erf
+from scipy.signal import gaussian
+
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy import units as u
 from astropy.coordinates import SkyCoord
+from spectral_cube import SpectralCube
 
 from mantipython import physics
 from misc_utils import get_pixel_scale
 import geometric_model as geomodel
+
+# For 3D plots
+from mayavi import mlab
+
 
 
 ancillary_data_path = "/home/rkarim/Research/Feedback/ancillary_data/"
@@ -19,6 +26,8 @@ if not os.path.isdir(ancillary_data_path):
     ancillary_data_path = "/home/ramsey/Documents/Research/Feedback/ancillary_data/"
 dust_path = f"{ancillary_data_path}dust/"
 herschel_path = f"{ancillary_data_path}herschel/"
+cii_path = f"{ancillary_data_path}sofia/"
+cii_cube = f"{cii_path}rcw49-cii.fits"
 
 def Draine_fn(Rv):
     if Rv in [3.1, 4.0, 5.5]:
@@ -60,7 +69,7 @@ def convert_ktoC(k):
 def get_val_at(wl, wl_array, val_array):
     return interp1d(wl_array, val_array)(wl)
 
-fit2p_filename = "RCW49large_2p.fits"
+fit2p_filename = "RCW49large_2p_2BAND_beta2.0.fits"
 
 def get_tau(filename=fit2p_filename, chisq_cut=None, flux_cut=None):
     with fits.open(herschel_path+filename) as hdul:
@@ -234,5 +243,145 @@ def integrate_shell_on_image():
     plt.colorbar()
     plt.show()
 
-if __name__ == "__main__":
-    integrate_shell_by_hand()
+
+"""
+%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+    Visualization
+%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&
+"""
+
+
+def make_cii_mom0():
+    """
+    Make & save these moment 0 maps:
+    1) entire velocity range
+    2) -12 to -8 km/s
+    3) -8 to -4 km/s
+    The last two are based on my 4/16/20 meeting with Xander and Maitraiyee
+    Maitraiyee's R-B uses these two ranges. Xander specifically suggested
+        -8 to -4 km/s as a mask for the dust optical depth.
+    """
+    raise RuntimeError("You already made these files on 4/22/20 on your laptop!")
+    with fits.open(cii_cube) as hdul:
+        h = hdul[0].header
+        w = WCS(h)
+        wflat = WCS(h, naxis=2)
+        data = hdul[0].data * u.K / (u.m / u.s)
+    cube = SpectralCube(data=data, wcs=w)
+    # First, save the whole moment 0 map
+    img1 = cube.moment(order=0)
+    h1 = fits.Header()
+    h1.update(wflat.to_header())
+    h1['COMMENT'] = "Full velocity-range moment 0 map from [CII]"
+    h1['BUNIT'] = 'K'
+    fits.writeto(f"{cii_path}mom0_fullrange.fits", img1.to_value(), h1)
+    del img1, h1
+    # Now make a subcubes from -12 to -8 and -8 to -4 km/s
+    img2 = cube.spectral_slab(-12*u.km/u.s, -8*u.km/u.s).moment(order=0)
+    h2 = fits.Header()
+    h2.update(wflat.to_header())
+    h2['COMMENT'] = "[CII] moment 0 from -12 km/s to -8 km/s"
+    h2['BUNIT'] = 'K'
+    fits.writeto(f"{cii_path}mom0_-12to-8.fits", img2.to_value(), h2)
+    del img2, h2
+    # And again
+    img2 = cube.spectral_slab(-8*u.km/u.s, -4*u.km/u.s).moment(order=0)
+    h2 = fits.Header()
+    h2.update(wflat.to_header())
+    h2['COMMENT'] = "[CII] moment 0 from -8 km/s to -4 km/s"
+    h2['BUNIT'] = 'K'
+    fits.writeto(f"{cii_path}mom0_-8to-4.fits", img2.to_value(), h2)
+    print("all done")
+
+
+def smooth_image(img, kernel_length=20, std=2):
+    """
+    Smooths a map with a kernel with length kernel_length and
+    standard deviation std
+    """
+    img = img.copy()
+    # Smooth spatial axes to avoid rough countours
+    smooth_kernel = gaussian(kernel_length, std)
+    smooth_kernel /= np.sum(smooth_kernel)
+    for i in range(img.shape[0]):
+        img[i, :] = np.convolve(img[i, :], smooth_kernel, mode='same')
+    for j in range(img.shape[1]):
+            img[:, j] = np.convolve(img[:, j], smooth_kernel, mode='same')
+    return img
+
+
+def prepare_img_for_plot(img, scale=np.arcsinh, low_cutoff=np.nanmedian):
+    img[img < low_cutoff(img)] = low_cutoff(img)
+    if scale is not None:
+        img = scale(img)
+    return img
+
+
+def make_surface_plot(img, warp_scale=15, colormap='blue-red'):
+    """
+    Make a surface plot with mayavi
+    """
+    img = img[::-1, ::-1]
+
+    yy, xx = np.meshgrid(*(np.arange(x) for x in img.shape), indexing='ij')
+    # xx, yy = xx.T, yy.T
+    mlab.surf(xx.T, yy.T, img.T, warp_scale=warp_scale, colormap=colormap)
+
+
+def load_cii(n):
+    """
+    Easily load in a [CII] moment 0 image
+    n is 0 for the full-range, 1 for [-12, -8], and 2 for [-8, -4]
+    """
+    if n == 0:
+        stub = "fullrange"
+    elif n == 1:
+        stub = "-12to-8"
+    elif n == 2:
+        stub = "-8to-4"
+    img, h = fits.getdata(f"{cii_path}mom0_{stub}.fits", header=True)
+    return img, h
+
+
+@mlab.animate(ui=False, delay=30)
+def loop_plots():
+    f = mlab.gcf()
+    x_arr = np.linspace(0, 2*np.pi, 50)
+    az_list = (np.sin(x_arr) * 15) + 45
+    i = 0
+    while i < len(az_list) - 1:
+        current_az = az_list[i]
+        mlab.view(azimuth=current_az, elevation=30)
+        f.scene.render()
+        # mlab.savefig(f"./figures/dust_surf_anim/anim_tausurf_{i:04d}.png")
+        i += 1
+        yield
+
+
+def make_integrateByHand_plot():
+    """
+    Some plots to illustrate the integrate_shell_by_hand method
+    Written 4/22/20
+    """
+    img = get_tau()
+    img = smooth_image(img, kernel_length=10, std=1.5)
+    img = prepare_img_for_plot(img, scale=np.log10)
+    make_surface_plot(img)
+    loop_plots()
+    mlab.show()
+
+
+
+
+sizemod = 2
+mlab.figure(size=(400*sizemod, 350*sizemod))
+img = get_tau()
+print(np.nanmin(img), np.nanmax(img))
+img = smooth_image(img, kernel_length=10, std=1.5)
+img = prepare_img_for_plot(img, scale=np.log10, low_cutoff=lambda x: np.nanmedian(x)/2)
+make_surface_plot(img, warp_scale=50)
+# The object "a" needs to NOT be garbage collected. There must be a reference!
+a = loop_plots()
+mlab.show()
